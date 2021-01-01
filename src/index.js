eaa7649d040830d2933fbe878fc33f7011c6f05d
ivanThePleasant <isevbusiness@gmail.com>
2021-01-01 22:47:00 +0000

    Remove dummy expense data in index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import { configureStore } from './store/configureStore'
-import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
 import 'react-dates/lib/css/_datepicker.css'
 // import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500}))
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
-store.dispatch(setTextFilter(''))
 
 
 // const state = store.getState()
